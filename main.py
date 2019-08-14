97581407361922d201c36d4636ac2060797c132e
Giovane Pianco <gopianco@hotmail.com>
2019-08-14 11:27:49 -0300

    refactor

---
+++ main.py
@@ -0,0 +1,26 @@
+from teste import Secao
+from os import getcwd
+
+def log_idclass():
+    idclass = Secao
+    idclass.url = 'https://192.168.0.179'
+    idclass.usuario = 'admin'
+    idclass.senha = 'admin'
+    idclass.nome_arquivo = r'\AFD00014003750030058.txt'
+    idclass.path = getcwd()
+
+    drive = idclass.config_driver(idclass)
+    idclass.log(idclass, drive)
+
+    idclass.verifica_arquivo(idclass)
+
+    idclass.baixar(idclass, drive)
+
+    idclass.fechar_navegador(idclass, drive)
+
+
+def main():
+    log_idclass()
+
+if __name__ == '__main__':
+    main()
